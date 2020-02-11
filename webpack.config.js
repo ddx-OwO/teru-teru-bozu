ba910b70d02f08281a16f837d6a6b0b6cbd4321f
Dewa Andhika Putra <dewaandhika18@gmail.com>
2020-02-11 16:42:13 +0700

    rename webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// module.exports = {
+//   theme: {
+//     extend: {}
+//   },
+//   variants: {},
+//   plugins: []
+// }
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'postcss-loader',
+        options: {
+          ident: 'postcss',
+          syntax: 'postcss-scss',
+          plugins: () => [
+            require('postcss-import'),
+            require('tailwindcss'),
+            require('autoprefixer')
+          ]
+        }
+      }
+    ]
+  }
+};
+
