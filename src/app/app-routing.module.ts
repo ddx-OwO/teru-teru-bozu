0d14213588d192ef9341c868d14d82f22c122d4d
Dewa Andhika Putra <dewaandhika18@gmail.com>
2020-02-15 07:41:55 +0700

    add lazy load routes for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home-page.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search-page.module').then(m => m.SearchPageModule)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
