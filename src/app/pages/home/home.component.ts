2cd9960c46107ad2d2dab699294bff34adb27ccb
Dewa Andhika Putra <dewaandhika18@gmail.com>
2020-02-17 01:54:29 +0700

    home page prototype

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,14 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   test = 'test123';
+  data = {
+    'result': [
+      {'id': 1},
+      {'id': 2},
+      {'id': 3},
+      {'id': 4}
+    ]
+  }
 
   ngOnInit(): void {
   }
